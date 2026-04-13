8c3e27d503c805df9f0136dfd7fc70af0ec9548d
DVitaliy <dynnik.vitaliy@gmail.com>
2026-04-13 12:27:01 +0300

    feat: integrate Grafana Faro for observability and error tracking
    - Added Faro SDK initialization in App.vue for monitoring application performance.
    - Configured tracing instrumentation to capture performance metrics.
    - Updated vite.config.ts to include Faro uploader plugin for source maps and error reporting.
    - Set up necessary configurations including app name, API key, and endpoint for Faro API.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,22 @@
 <script setup lang="ts">
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+initializeFaro({
+  url: 'https://faro-collector-prod-eu-west-2.grafana.net/collect/90e043e1959496cc5eca0cbd890c276a',
+  app: {
+    name: 'flippa',
+    version: '1.0.0',
+    environment: 'production'
+  },
+
+  instrumentations: [
+    ...getWebInstrumentations(),
+
+    new TracingInstrumentation(),
+  ],
+});
+
 console.log(`\x1b[101mDear hackers, a small disappointment awaits you: this is only a demo project.
 Most of the content here uses mock data, and database access is either temporary, read-only, or will be removed entirely after review.`)
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
+import faroUploader from "@grafana/faro-rollup-plugin";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+      tailwindcss(),
+      faroUploader({
+        appName: "flippa",
+        endpoint: "https://faro-api-prod-eu-west-2.grafana.net/faro/api/v1",
+        apiKey: env.GRAFANA_FARO_API_KEY,
+        appId: "5512",
+        stackId: "1574680",
+        verbose: true,
+        gzipContents: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-})
+  };
+});
